a450946487a4a90ae2afb757f5a06e1ae4453007
Nir Tamir <nirtamir2@gmail.com>
2024-07-20 15:13:42 +0300

    video

--- src/components/GamesCarousel.tsx
+++ src/components/GamesCarousel.tsx
@@ -19,47 +19,72 @@ export function GamesCarousel(props: { games: FetchGameDataResult }) {
       orientation="vertical"
     >
       <CarouselContent>
-        {games.map((game) => (
-          <CarouselItem key={game.id}>
-            <div key={game.id} className="relative size-full">
-              {game.gameData.background_image == null ? (
-                <div className="size-full bg-background"></div>
-              ) : (
-                <Image
-                  fill
-                  style={{
-                    objectFit: "cover",
-                    userSelect: "none",
-                  }}
-                  sizes="100vw"
-                  src={game.gameData.background_image}
-                  alt=""
+        {games.map((game) => {
+          const fullImage =
+            game.gameData.background_image == null ? (
+              <div className="size-full bg-background"></div>
+            ) : (
+              <Image
+                fill
+                style={{
+                  objectFit: "cover",
+                  userSelect: "none",
+                }}
+                sizes="100vw"
+                src={game.gameData.background_image}
+                alt=""
+              />
+            );
+          const fullVideo =
+            game.gameMovies.results[0] == null ||
+            game.gameMovies.results[0].data.max == null ? (
+              fullImage
+            ) : (
+              // TODO: play when in view
+              <video
+                playsInline
+                autoPlay
+                muted
+                loop
+                preload="auto"
+                className="h-full object-cover"
+              >
+                <source
+                  src={game.gameMovies.results[0].data.max}
+                  type="video/mp4"
                 />
-              )}
-              <div className="absolute bottom-0 z-10 w-full bg-gradient-to-b from-transparent via-background via-50% to-background p-8 sm:p-24">
-                <div>
-                  <div className="inline pb-4 pr-4 text-3xl font-semibold text-white">
-                    {game.gameData.name}
+                {fullImage}
+              </video>
+            );
+          return (
+            <CarouselItem key={game.id}>
+              <div key={game.id} className="relative size-full">
+                {fullVideo}
+                <div className="absolute bottom-0 z-10 w-full bg-gradient-to-b from-transparent via-background via-50% to-background p-8 sm:p-24">
+                  <div>
+                    <div className="inline pb-4 pr-4 text-3xl font-semibold text-white">
+                      {game.gameData.name}
+                    </div>
+                    <span className="rounded bg-green-500 p-1 text-xs text-black">
+                      {Math.round(game.gameData.rating * 20)}
+                    </span>
                   </div>
-                  <span className="rounded bg-green-500 p-1 text-xs text-black">
-                    {Math.round(game.gameData.rating * 20)}
-                  </span>
+                  <div className="text-lg text-white">{game.gameData.slug}</div>
+                  <div className="flex flex-wrap gap-4">
+                    {game.gameData.genres.map((genre) => {
+                      return (
+                        <div key={genre.id} className="text-lg text-white">
+                          {genre.name}
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="text-lg text-white">{game.explanation}</div>
                 </div>
-                <div className="text-lg text-white">{game.gameData.slug}</div>
-                <div className="flex flex-wrap gap-4">
-                  {game.gameData.genres.map((genre) => {
-                    return (
-                      <div key={genre.id} className="text-lg text-white">
-                        {genre.name}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="text-lg text-white">{game.explanation}</div>
               </div>
-            </div>
-          </CarouselItem>
-        ))}
+            </CarouselItem>
+          );
+        })}
       </CarouselContent>
       <div className="absolute -left-10 top-1/2 hidden bg-green-200 md:block">
         <CarouselPrevious />
