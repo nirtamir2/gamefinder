da4060316c2ad6c90f5b432889b9d6022613a3aa
Nir Tamir <nirtamir2@gmail.com>
2024-08-02 18:38:48 +0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={spaceMono.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
