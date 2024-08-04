f8ddd95d86bcdccf937c468236781ac3b7ab4653
Nir Tamir <nirtamir2@gmail.com>
2024-08-04 18:07:08 +0300

    eslint and deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,9 @@
-import nirtamir2, { GLOB_TS, GLOB_TSX } from "@nirtamir2/eslint-config";
-import sortDestructureKeysPlugin from "eslint-plugin-sort-destructure-keys-typescript";
+import nirtamir2 from "@nirtamir2/eslint-config";
 
-export default nirtamir2(
-  {
-    formatters: true,
-    react: true,
-  },
-  {
-    files: [GLOB_TS, GLOB_TSX],
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      "sort-destructure-keys-typescript": sortDestructureKeysPlugin,
-    },
-    rules: {
-      "sort-destructure-keys-typescript/sort-destructure-keys-by-type": "error",
-    },
-  },
-);
+export default nirtamir2({
+  formatters: true,
+  react: true,
+  typescript: true,
+  tailwindcss: true,
+  jsx: true,
+});
