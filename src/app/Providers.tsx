d22c273dda0f54cf742f129c1028c1f3623fde29
Nir Tamir <nirtamir2@gmail.com>
2024-08-05 16:02:36 +0300

    get query client

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,7 +3,7 @@
 import type * as React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { getQueryClient } from "@/app/get-query-client";
+import { getQueryClient } from "@/app/getQueryClient";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
