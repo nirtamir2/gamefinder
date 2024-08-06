f532aec929eb7b5744548532c68c6473f6650e1e
Nir Tamir <nirtamir2@gmail.com>
2024-08-06 12:53:41 +0300

    wip new design

---
+++ src/app/PlatformsDrawer.tsx
@@ -0,0 +1,3 @@
+export function PlatformsDrawer() {
+  return <div>All Platforms</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,36 @@
 import Image from "next/image";
-import { SearchParametersProviderProvider } from "@/app/GamesProvider";
+import { PlatformsDrawer } from "@/app/PlatformsDrawer";
 import cubeImageSrc from "@/assets/cube.png";
-import { DiscoverGamesDrawer } from "@/components/DiscoverGamesDrawer";
 import { Button } from "@/components/ui/Button";
+import { TextArea } from "@/components/ui/TextArea";
 import { Icon } from "@/components/ui/icons/Icon";
 
 export default async function Home() {
   return (
     <div className="flex h-dvh w-full">
-      <div className="flex w-full flex-col items-center justify-center">
-        <Image src={cubeImageSrc} height={278} width={278} alt="" />
-        <div className="relative -top-8 flex flex-col">
-          <h1 className="pt-2 text-center text-lg font-bold">gameplays.io</h1>
-          <h2 className="text-balance px-8 pt-2 text-center text-sm font-bold">
-            Use AI to find your next favorite videogame
-          </h2>
-          <div className="flex justify-center px-8 pt-8">
-            <div className="w-full sm:max-w-56">
-              <SearchParametersProviderProvider>
-                <DiscoverGamesDrawer
-                  triggerAsChild
-                  trigger={<Button>Discover Games</Button>}
-                />
-              </SearchParametersProviderProvider>
-            </div>
+      <div className="flex w-full items-center">
+        <div className="container w-full px-8 pb-32">
+          <div className="flex justify-center">
+            <Image src={cubeImageSrc} height={278} width={278} alt="" />
           </div>
+          <div className="relative -top-12">
+            <h1 className="pt-2 text-center text-lg font-bold text-white">
+              gameplays.io
+            </h1>
+          </div>
+          <form className="flex flex-col items-center justify-center gap-8">
+            <TextArea
+              required
+              id="likedGames"
+              name="likedGames"
+              rows={6}
+              placeholder="Example: I like challenging puzzle games with adventures like legend of zelda or Tunic"
+            />
+            <PlatformsDrawer />
+            <div className="w-full sm:max-w-56">
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
         </div>
       </div>
       <footer className="fixed bottom-16 flex w-full items-center justify-center gap-2">
