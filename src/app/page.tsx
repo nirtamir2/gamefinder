faacdf853fc15be33e9d954b67038ee9d99241b8
Ariel Verber <opiedesigns@gmail.com>
2024-08-08 00:04:12 +0300

    design fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,19 @@ import { GameProvider } from "@/components/providers/GameProvider";
 
 export default async function Home() {
   return (
-    <div className="flex h-dvh w-full">
-      <div className="flex w-full items-center">
-        <div className="container relative -top-16 w-full px-8 pb-32">
-          <div className="flex justify-center">
-            <Image src={cubeImageSrc} height={278} width={278} alt="" />
-          </div>
-          <div className="relative -top-12">
-            <h1 className="pt-2 text-center text-lg font-bold text-white">
-              gameplays.io
-            </h1>
-          </div>
-          <GameProvider>
-            <MainPageForm />
-          </GameProvider>
+    <div className="flex h-dvh w-full flex-col items-center justify-center">
+      <div className="container relative w-full px-8 pb-32">
+        <div className="flex justify-center">
+          <Image src={cubeImageSrc} height={200} width={200} alt="" />
         </div>
+        <div className="relative -top-12">
+          <h1 className="pt-2 text-center text-lg font-bold text-white">
+            gameplays.io
+          </h1>
+        </div>
+        <GameProvider>
+          <MainPageForm />
+        </GameProvider>
       </div>
       <PoweredByGeminiFooter />
     </div>
